48bd9467d3b95b3915d9ff71b21f718c82b922d0
dbcalitis <daria_calitis@yahoo.com>
2026-01-17 23:38:35 -0500

    feat: animations :3

--- orthography.c
+++ orthography.c
@@ -1,6 +1,10 @@
 #include "orthography.h"
 #include <stdlib.h>
 
+//~ daria: Render/Animation
+#define MAX_ANIMATIONS 1
+#define ANIMATION_CAPACITY 5
+
 //~ daria: Audio
 #define MAX_SOUNDS 1
 #define SOUND_EFFECT_CAPACITY 5 // used in Entity
@@ -21,6 +25,63 @@ char * sound_names[MAX_SOUNDS] =
     SOUNDS_LIST
 #undef SOUND
 };
+
+//~ daria: Animations
+// TODO(daria): place this elsewhere
+typedef struct AnimationFrame AnimationFrame;
+struct AnimationFrame
+{
+    U64 sprite_map_index; // starting frame index
+    U32 duration; // per frame
+};
+
+typedef struct Animation Animation;
+struct Animation
+{
+    Texture2D texture;
+
+    U32 x_cell_count;
+    U32 y_cell_count;
+
+    AnimationFrame *frames;
+    // left to right, top to bottom
+    U32 frames_size;
+    U32 current_frame;
+    U32 frame_duration;
+    U32 cell_size; // px
+};
+
+internal Animation
+animation_load(
+        Arena *arena,
+        Texture2D texture,
+        U8 frames_size,
+        U8 cell_size)
+{
+    // Animation
+    Animation a =
+    {
+        .texture = texture,
+
+        .x_cell_count = (F32) a.texture.width / cell_size,
+        .y_cell_count = (F32) a.texture.height / cell_size,
+        .frames = arena_push_array(arena, AnimationFrame, frames_size),
+        .frames_size = frames_size,
+        .cell_size = cell_size
+    };
+
+    return a;
+}
+
+internal void
+animation_next_frame(
+        Animation *a)
+{
+    if (a->frame_duration >= a->frames[a->current_frame].duration)
+    {
+        a->current_frame = (a->current_frame + 1) % a->frames_size;
+    }
+}
 
 //~ angn: Inputs
 typedef enum InputState : U8
@@ -63,6 +124,8 @@ typedef enum : U64
     EntityFlagsIndex_Alive,
     EntityFlagsIndex_Apply_Velocity,
     EntityFlagsIndex_WASD_Motion,
+    EntityFlagsIndex_Player,
+    EntityFlagsIndex_RenderTexture,
 } EntityFlagsIndex;
 
 #define EntityFlags_Assert_IndexValid(index) do { \
@@ -106,6 +169,17 @@ entity_flags_contains(
     return(flags->f[quo] & (1 << rem));
 }
 
+//~ daria: entity states
+typedef U64 EntityState;
+typedef enum : EntityState
+{
+    PlayerState_Idle,
+    PlayerState_Down,
+    PlayerState_Up,
+    PlayerState_Left,
+    PlayerState_Right,
+} PlayerState;
+
 //~ angn: Entities
 typedef struct Entity Entity;
 struct Entity
@@ -117,10 +191,13 @@ struct Entity
     Vector2 position;
     Vector2 velocity;
 
+    EntityState player_state;
+
     //- angn: TODO: audio
     Sound sound_effects[SOUND_EFFECT_CAPACITY];
 
     //- angn: TODO: render / animations
+    Animation animations[ANIMATION_CAPACITY];
 };
 
 //~ angn: Game
@@ -135,6 +212,7 @@ struct Game
     U64 entities_count;
 
     Sound sound_effects[MAX_SOUNDS];
+    Animation animations[MAX_ANIMATIONS];
 };
 
 internal Entity *
@@ -217,26 +295,46 @@ game_update(
         {
             Vector2 dir = {0};
 
+            PlayerState old_state = entity->player_state;
+
+            // daria: NOTE: this assumes it's a player
+            // TODO(daria): determine entity type
             if(inputs[InputTypes_W] & InputState_Down)
             {
                 dir.y -= 1.0f;
                 PlaySound(entity->sound_effects[0]);
+                entity->player_state = PlayerState_Up;
             }
-            if(inputs[InputTypes_S] & InputState_Down)
+            else if(inputs[InputTypes_S] & InputState_Down)
             {
                 dir.y += 1.0f;
                 PlaySound(entity->sound_effects[0]);
+                entity->player_state = PlayerState_Down;
             }
-            if(inputs[InputTypes_D] & InputState_Down)
+            else if(inputs[InputTypes_D] & InputState_Down)
             {
                 dir.x += 1.0f;
                 PlaySound(entity->sound_effects[0]);
+                entity->player_state = PlayerState_Right;
             }
-            if(inputs[InputTypes_A] & InputState_Down)
+            else if(inputs[InputTypes_A] & InputState_Down)
             {
                 dir.x -= 1.0f;
                 PlaySound(entity->sound_effects[0]);
+                entity->player_state = PlayerState_Left;
             }
+            else
+            // Check if the player is idle
+            {
+                entity->player_state = PlayerState_Idle;
+            }
+
+            if (old_state != entity->player_state)
+            {
+                entity->animations[old_state].frame_duration = 0;
+            }
+
+            entity->animations[entity->player_state].frame_duration++;
 
             dir = Vector2ClampValue(dir, 0.0f, 1.0f);
             entity->velocity =
@@ -291,6 +389,28 @@ main(
         game->sound_effects[i] = LoadSound(sound_names[i]);
     }
 
+    //- daria: animations
+    // TODO(daria): have separate spritesheets or a single one
+    // add arena param load
+    Texture2D player_texture = LoadTexture("textures/Creature.png");
+    Animation player_animation_down = animation_load(global_arena, player_texture, 1, 32);
+    player_animation_down.frames[0] = (AnimationFrame) { .sprite_map_index = 0, .duration = 1 };
+
+    Animation player_animation_up = animation_load(global_arena, player_texture, 1, 32);
+    player_animation_up.frames[0] = (AnimationFrame) { .sprite_map_index = 1, .duration = 1 };
+
+    Animation player_animation_left = animation_load(global_arena, player_texture, 1, 32);
+    player_animation_left.frames[0] = (AnimationFrame) { .sprite_map_index = 2, .duration = 1 };
+
+    Animation player_animation_right = animation_load(global_arena, player_texture, 1, 32);
+    player_animation_right.frames[0] = (AnimationFrame) { .sprite_map_index = 3, .duration = 1 };
+
+    Animation player_animation_idle = animation_load(global_arena, player_texture, 4, 32);
+    player_animation_idle.frames[0] = (AnimationFrame) { .sprite_map_index = 0, .duration = 1 };
+    player_animation_idle.frames[1] = (AnimationFrame) { .sprite_map_index = 1, .duration = 1 };
+    player_animation_idle.frames[2] = (AnimationFrame) { .sprite_map_index = 2, .duration = 1 };
+    player_animation_idle.frames[3] = (AnimationFrame) { .sprite_map_index = 3, .duration = 1 };
+
     //- angn: fixed timestep
     // angn: NOTE: is this update rate too high?
     F32 dt_fixed = 1.0f / 60.0f; // update rate
@@ -306,6 +426,29 @@ main(
 
     //- angn: entities
     {
+        Entity *player = alloc_entity(game);
+        Assert(player);
+        printf("%lu\n", player - game->entities);
+        entity_flags_set(&player->flags, EntityFlagsIndex_WASD_Motion);
+        entity_flags_set(&player->flags, EntityFlagsIndex_Apply_Velocity);
+        entity_flags_set(&player->flags, EntityFlagsIndex_Player);
+        entity_flags_set(&player->flags, EntityFlagsIndex_RenderTexture);
+        player->position = (Vector2){ 0.0, Cast(F32, game->screen.y) * 0.1 };
+        for (int i = 0; i < MAX_SOUNDS && i < SOUND_EFFECT_CAPACITY; i++)
+        {
+            player->sound_effects[i] = LoadSoundAlias(game->sound_effects[i]);
+        }
+        player->player_state = PlayerState_Up;
+
+        player->animations[PlayerState_Down] = player_animation_down;
+        player->animations[PlayerState_Up] = player_animation_up;
+        player->animations[PlayerState_Left] = player_animation_left;
+        player->animations[PlayerState_Right] = player_animation_right;
+        player->animations[PlayerState_Idle] = player_animation_idle;
+    }
+
+    /*
+    {
         Entity *ball = alloc_entity(game);
         Assert(ball);
         printf("%lu\n", ball - game->entities);
@@ -317,6 +460,7 @@ main(
             ball->sound_effects[i] = LoadSoundAlias(game->sound_effects[i]);
         }
     }
+    */
 
     {
         Entity *ball = alloc_entity(game);
@@ -381,8 +525,53 @@ main(
                     continue;
                 }
 
-                // angn: TODO: only render if asked
-                DrawCircleV(entity->position, 25.0f, SKYBLUE);
+                //- daria: render entity
+                if(entity_flags_contains(&entity->flags, EntityFlagsIndex_RenderTexture))
+                {
+                    Animation *animation = &entity->animations[entity->player_state];
+                    AnimationFrame *frame = &animation->frames[animation->current_frame];
+
+                    // daria: TODO: precompute?
+                    U32 row_size = animation->texture.width / animation->cell_size;
+
+                    // daria: NOTE: the y value is hard coded
+                    Rectangle frame_rec =
+                    {
+                        .x = (F32)((frame->sprite_map_index % row_size) * animation->cell_size),
+                        .y = (F32)((frame->sprite_map_index / row_size) * animation->cell_size),
+                        .width = animation->cell_size,
+                        .height = animation->cell_size,
+                    };
+
+                    Rectangle dest_rec =
+                    {
+                        .x = entity->position.x,
+                        .y = entity->position.y,
+                        .width = 128,
+                        .height = 128
+                    };
+
+                    Vector2 origin = (Vector2)
+                    {
+                        .x = animation->cell_size,
+                        .y = animation->cell_size
+                    };
+
+                    DrawTexturePro(
+                            entity->animations[entity->player_state].texture,
+                            frame_rec,
+                            dest_rec,
+                            origin,
+                            0,
+                            WHITE);
+
+                    // daria: TODO: go to next frame
+                    animation_next_frame(animation);
+                }
+                else
+                {
+                    DrawCircleV(entity->position, 25.0f, SKYBLUE);
+                }
             }
         }
         EndDrawing();
@@ -396,7 +585,7 @@ main(
     {
         UnloadSound(game->sound_effects[i]);
     }
-    CloseAudioDevice();
 
+    CloseAudioDevice();
     return(0);
 }
